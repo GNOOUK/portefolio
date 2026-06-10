f3255fc513e8da35000876ba5bb3da11337961c6
GNOOUK <167510224+GNOOUK@users.noreply.github.com>
2026-06-10 14:19:58 +0200

    Create data.js

---
+++ data.js
@@ -0,0 +1,14 @@
+let projects = [
+  {title:"Zombie Tycoon", desc:"Jeu Roblox survie zombies"},
+  {title:"UnderWater Power", desc:"Aventure sous-marine"}
+];
+
+let feedbacks = [
+  {user:"Alex", text:"Très bon dev !"},
+  {user:"Mike", text:"Travail propre et rapide"}
+];
+
+let socials = [
+  {name:"YouTube", link:"#"},
+  {name:"TikTok", link:"#"}
+];
